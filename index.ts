669b624a95f483231b0f83e25954ffedf9f03fab
John Daly <jd@interlink.team>
2021-12-07 12:50:56 +0100

    fix for uppercase letters and got rid of ascii map

--- index.ts
+++ index.ts
@@ -89,15 +89,15 @@ function aufgabe_3(data, sum) {
     let u_count = 0;
 
     for (const char of data) {
-        if (char === 'a') {
+        if (char === 'a' || char === 'A') {
             a_count += 1;
-        } else if (char === 'e') {
+        } else if (char === 'e' || char === 'E') {
             e_count += 1;
-        } else if (char === 'i') {
+        } else if (char === 'i' || char === 'I') {
             i_count += 1;
-        } else if (char === 'o') {
+        } else if (char === 'o' || char === 'O') {
             o_count += 1;
-        } else if (char === 'u') {
+        } else if (char === 'u' || char === 'U') {
             u_count += 1;
         }
     }
@@ -189,40 +189,15 @@ function aufgabe_4_a(sentences) {
     console.log('größte 3 Werte in der Reihenfolge ihres Vorkommens abzüglich Index: ');
     console.log(JSON.stringify(sums_in_order_min_i));
 
-    //let first_10_in_order: number[] = get_first_n_in_order(first_10, sums);
-
     return first_10;
 }
 
 function aufgabe_4_b(sums) {
     console.log('\n\n\t Section b.');
 
-    const ascii = {
-        "31": "",      "32": " ",     "33": "!",     "34": "\"",    "35": "#",    
-        "36": "$",     "37": "%",     "38": "&",     "39": "'",     "40": "(",    
-        "41": ")",     "42": "*",     "43": "+",     "44": ",",     "45": "-",    
-        "46": ".",     "47": "/",     "48": "0",     "49": "1",     "50": "2",    
-        "51": "3",     "52": "4",     "53": "5",     "54": "6",     "55": "7",    
-        "56": "8",     "57": "9",     "58": ":",     "59": ";",     "60": "<",    
-        "61": "=",     "62": ">",     "63": "?",     "64": "@",     "65": "A",    
-        "66": "B",     "67": "C",     "68": "D",     "69": "E",     "70": "F",    
-        "71": "G",     "72": "H",     "73": "I",     "74": "J",     "75": "K",    
-        "76": "L",     "77": "M",     "78": "N",     "79": "O",     "80": "P",    
-        "81": "Q",     "82": "R",     "83": "S",     "84": "T",     "85": "U",    
-        "86": "V",     "87": "W",     "88": "X",     "89": "Y",     "90": "Z",    
-        "91": "[",     "92": "\\",    "93": "]",     "94": "^",     "95": "_",    
-        "96": "`",     "97": "a",     "98": "b",     "99": "c",     "100": "d",    
-        "101": "e",    "102": "f",    "103": "g",    "104": "h",    "105": "i",    
-        "106": "j",    "107": "k",    "108": "l",    "109": "m",    "110": "n",    
-        "111": "o",    "112": "p",    "113": "q",    "114": "r",    "115": "s",    
-        "116": "t",    "117": "u",    "118": "v",    "119": "w",    "120": "x",    
-        "121": "y",    "122": "z",    "123": "{",    "124": "|",    "125": "}",    
-        "126": "~",    "127": ""
-    };
-    
     const chars: string[] = []
     for (const sum of sums) {
-        const char = ascii[sum.toString()];
+        const char = String.fromCharCode(sum);
         chars.push(char);
     }
     const solution = chars.join('');
